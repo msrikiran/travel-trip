8dc8654fe270baa2337938a68247971f049baa39
Iamsrikiran <118716628+Iamsrikiran@users.noreply.github.com>
2024-08-26 21:58:45 +0530

    4 components are added and length 20 dropdown added dynamically

--- src/App.js
+++ src/App.js
@@ -1,5 +1,70 @@
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: false },
+  { id: 3, description: 'Charger', quantity: 7, packed: true },
+];
+
 function App() {
-  return <h1> Travel list</h1>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌲 Far Away 💼💼</h1>;
+}
+
+function Form() {
+  return (
+    <form className="add-form">
+      <h3>What do you need for your trip ?</h3>
+      <select name="" id="">
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="item..." />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul className="list">
+        {initialItems.map((item) => (
+          <Item packItem={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item(props) {
+  const { description, quantity, packed } = props.packItem;
+  return (
+    <li>
+      <span style={packed ? { textDecoration: 'line-through' } : {}}>
+        {quantity + ' '} {description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
 }
 
 export default App;
